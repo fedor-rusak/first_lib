1faacfc398ec094b108b2dacb742f1ae7c772923
Fedor Rusak <fedor.rusak@gmail.com>
2018-08-25 13:15:49 +0300

    Added analysis of queue family properties

--- src/vulkan_renderer/mod.rs
+++ src/vulkan_renderer/mod.rs
@@ -42,8 +42,9 @@ use self::vulkan_types::*;
 ///
 /// 3) VkInstance is created (thing that is responsible for stateful communication with Vulkan-compatibe devices)
 /// 4) vkEnumeratePhysicalDevices is called two times in order to get data about first Vulkan-compatible physical device
+/// 5) vkGetPhysicalDeviceQueueFamilyProperties to find a queueFamily that can be used for rendering
 ///
-/// 5) vkDestroyInstance is called to clean up everything
+/// 6) vkDestroyInstance is called to clean up everything
 ///
 pub fn main() -> i32 {
 	println!("Hello from rust-ffi-glfw!");
@@ -100,7 +101,7 @@ pub fn main() -> i32 {
             let create_info = VkInstanceCreateInfo {
                 s_type: VkStructureType::InstanceCreateInfo,
                 p_next: ptr::null(),
-                flags: VkFlags::Empty,
+                flags: flags::EMPTY,
                 p_application_info: &appinfo,
                 pp_enabled_layer_names: ptr::null(),
                 enabled_layer_count: 0 as u32,
@@ -138,6 +139,7 @@ pub fn main() -> i32 {
             println!("Vulkan physical device count: {}", device_count);
 
             let mut physical_devices = Vec::<VkPhysicalDevice>::with_capacity(device_count as usize);
+
             let physical_device_enumarate_result = 
                 (enumerate_devices_function)(instance, &mut device_count, physical_devices.as_mut_ptr());
 
@@ -149,8 +151,41 @@ pub fn main() -> i32 {
                 return -1
             }
 
+            //driver does not know about our internal counter in Vec
+            physical_devices.set_len(device_count as usize);
+            let chosen_physical_device = physical_devices[0];
+
             //vkEnumeratePhysicalDevices part END
 
+
+            //vkGetPhysicalDeviceQueueFamilyProperties part START
+
+            let string = CString::new("vkGetPhysicalDeviceQueueFamilyProperties").unwrap(); //tricky stuff. If written in one line string would vanish!
+            let function_name = string.as_ptr() as *const c_char;
+
+            //this is some black magic thing
+            let get_queue_family_properties_proc = glfwGetInstanceProcAddress(ptr::null_mut(), function_name);
+            let get_queue_family_properties_function: vkGetPhysicalDeviceQueueFamilyProperties = mem::transmute(get_queue_family_properties_proc);
+
+            let mut queue_family_count = mem::uninitialized();
+            (get_queue_family_properties_function)(chosen_physical_device, &mut queue_family_count, ptr::null_mut());
+
+            println!("On first physical device we have this queue family count: {}", queue_family_count);
+
+            let mut queue_families_properties = Vec::<VkQueueFamilyProperties>::with_capacity(queue_family_count as usize);
+
+            (get_queue_family_properties_function)(chosen_physical_device, &mut queue_family_count, queue_families_properties.as_mut_ptr());
+
+            //driver does not know about our internal counter in Vec
+            queue_families_properties.set_len(queue_family_count as usize);
+
+            for i in 0..queue_families_properties.len() {
+                println!("Queue family number {} has flags = {:b} and queue count = {}",
+                    i, queue_families_properties[i].queue_flags, queue_families_properties[i].queue_count);
+
+            }
+
+            //vkGetPhysicalDeviceQueueFamilyProperties part END
 
 
             // let string = CString::new("vkCreateDevice").unwrap(); //tricky stuff. If written in one line string would vanish!
--- src/vulkan_renderer/vulkan_types.rs
+++ src/vulkan_renderer/vulkan_types.rs
@@ -35,9 +35,18 @@ pub type uint8_t = u8;
 pub type c_float = f32;
 pub type int32_t = i32;
 
+pub type Flags = u32;
 pub type Bool32 = uint32_t;
 pub type DeviceSize = uint64_t;
 pub type SampleMask = uint32_t;
+
+pub mod flags {
+    pub const EMPTY: u32 = 0;
+    pub const QUEUE_GRAPHICS_BIT: u32 = 1;
+    pub const QUEUE_COMPUTE_BIT: u32 = 2;
+    pub const QUEUE_TRANSFER_BIT: u32 = 4;
+    pub const QUEUE_SPARSE_BINDING_BIT: u32 = 8;
+}
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -140,7 +149,7 @@ pub enum VkResult {
 pub struct VkInstanceCreateInfo {
     pub s_type: VkStructureType,
     pub p_next: *const c_void,
-    pub flags: VkFlags,
+    pub flags: Flags,
     pub p_application_info: *const VkApplicationInfo,
     pub enabled_layer_count: uint32_t,
     pub pp_enabled_layer_names: *const *const c_char,
@@ -161,12 +170,6 @@ pub struct VkApplicationInfo {
     pub api_version: uint32_t,
 }
 
-#[repr(C)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum VkFlags {
-    Empty = 0
-}
-
 #[allow(missing_copy_implementations)]
 pub enum VkAllocationCallbacks {}
 
@@ -175,7 +178,7 @@ pub enum VkAllocationCallbacks {}
 pub struct DeviceCreateInfo {
     pub s_type: VkStructureType,
     pub p_next: *const c_void,
-    pub flags: VkFlags,
+    pub flags: Flags,
     pub queue_create_info_count: uint32_t,
     pub p_queue_create_infos: *const DeviceQueueCreateInfo,
     pub enabled_layer_count: uint32_t,
@@ -190,69 +193,86 @@ pub struct DeviceCreateInfo {
 pub struct DeviceQueueCreateInfo {
     pub s_type: VkStructureType,
     pub p_next: *const c_void,
-    pub flags: VkFlags,
+    pub flags: Flags,
     pub queue_family_index: uint32_t,
     pub queue_count: uint32_t,
     pub p_queue_priorities: *const c_float,
 }
 
  #[repr(C)]
-    pub struct PhysicalDeviceFeatures {
-        pub robust_buffer_access: Bool32,
-        pub full_draw_index_uint32: Bool32,
-        pub image_cube_array: Bool32,
-        pub independent_blend: Bool32,
-        pub geometry_shader: Bool32,
-        pub tessellation_shader: Bool32,
-        pub sample_rate_shading: Bool32,
-        pub dual_src_blend: Bool32,
-        pub logic_op: Bool32,
-        pub multi_draw_indirect: Bool32,
-        pub draw_indirect_first_instance: Bool32,
-        pub depth_clamp: Bool32,
-        pub depth_bias_clamp: Bool32,
-        pub fill_mode_non_solid: Bool32,
-        pub depth_bounds: Bool32,
-        pub wide_lines: Bool32,
-        pub large_points: Bool32,
-        pub alpha_to_one: Bool32,
-        pub multi_viewport: Bool32,
-        pub sampler_anisotropy: Bool32,
-        pub texture_compression_etc2: Bool32,
-        pub texture_compression_astc_ldr: Bool32,
-        pub texture_compression_bc: Bool32,
-        pub occlusion_query_precise: Bool32,
-        pub pipeline_statistics_query: Bool32,
-        pub vertex_pipeline_stores_and_atomics: Bool32,
-        pub fragment_stores_and_atomics: Bool32,
-        pub shader_tessellation_and_geometry_point_size: Bool32,
-        pub shader_image_gather_extended: Bool32,
-        pub shader_storage_image_extended_formats: Bool32,
-        pub shader_storage_image_multisample: Bool32,
-        pub shader_storage_image_read_without_format: Bool32,
-        pub shader_storage_image_write_without_format: Bool32,
-        pub shader_uniform_buffer_array_dynamic_indexing: Bool32,
-        pub shader_sampled_image_array_dynamic_indexing: Bool32,
-        pub shader_storage_buffer_array_dynamic_indexing: Bool32,
-        pub shader_storage_image_array_dynamic_indexing: Bool32,
-        pub shader_clip_distance: Bool32,
-        pub shader_cull_distance: Bool32,
-        pub shader_float64: Bool32,
-        pub shader_int64: Bool32,
-        pub shader_int16: Bool32,
-        pub shader_resource_residency: Bool32,
-        pub shader_resource_min_lod: Bool32,
-        pub sparse_binding: Bool32,
-        pub sparse_residency_buffer: Bool32,
-        pub sparse_residency_image2d: Bool32,
-        pub sparse_residency_image3d: Bool32,
-        pub sparse_residency2samples: Bool32,
-        pub sparse_residency4samples: Bool32,
-        pub sparse_residency8samples: Bool32,
-        pub sparse_residency16samples: Bool32,
-        pub sparse_residency_aliased: Bool32,
-        pub variable_multisample_rate: Bool32,
-        pub inherited_queries: Bool32,
+pub struct PhysicalDeviceFeatures {
+    pub robust_buffer_access: Bool32,
+    pub full_draw_index_uint32: Bool32,
+    pub image_cube_array: Bool32,
+    pub independent_blend: Bool32,
+    pub geometry_shader: Bool32,
+    pub tessellation_shader: Bool32,
+    pub sample_rate_shading: Bool32,
+    pub dual_src_blend: Bool32,
+    pub logic_op: Bool32,
+    pub multi_draw_indirect: Bool32,
+    pub draw_indirect_first_instance: Bool32,
+    pub depth_clamp: Bool32,
+    pub depth_bias_clamp: Bool32,
+    pub fill_mode_non_solid: Bool32,
+    pub depth_bounds: Bool32,
+    pub wide_lines: Bool32,
+    pub large_points: Bool32,
+    pub alpha_to_one: Bool32,
+    pub multi_viewport: Bool32,
+    pub sampler_anisotropy: Bool32,
+    pub texture_compression_etc2: Bool32,
+    pub texture_compression_astc_ldr: Bool32,
+    pub texture_compression_bc: Bool32,
+    pub occlusion_query_precise: Bool32,
+    pub pipeline_statistics_query: Bool32,
+    pub vertex_pipeline_stores_and_atomics: Bool32,
+    pub fragment_stores_and_atomics: Bool32,
+    pub shader_tessellation_and_geometry_point_size: Bool32,
+    pub shader_image_gather_extended: Bool32,
+    pub shader_storage_image_extended_formats: Bool32,
+    pub shader_storage_image_multisample: Bool32,
+    pub shader_storage_image_read_without_format: Bool32,
+    pub shader_storage_image_write_without_format: Bool32,
+    pub shader_uniform_buffer_array_dynamic_indexing: Bool32,
+    pub shader_sampled_image_array_dynamic_indexing: Bool32,
+    pub shader_storage_buffer_array_dynamic_indexing: Bool32,
+    pub shader_storage_image_array_dynamic_indexing: Bool32,
+    pub shader_clip_distance: Bool32,
+    pub shader_cull_distance: Bool32,
+    pub shader_float64: Bool32,
+    pub shader_int64: Bool32,
+    pub shader_int16: Bool32,
+    pub shader_resource_residency: Bool32,
+    pub shader_resource_min_lod: Bool32,
+    pub sparse_binding: Bool32,
+    pub sparse_residency_buffer: Bool32,
+    pub sparse_residency_image2d: Bool32,
+    pub sparse_residency_image3d: Bool32,
+    pub sparse_residency2samples: Bool32,
+    pub sparse_residency4samples: Bool32,
+    pub sparse_residency8samples: Bool32,
+    pub sparse_residency16samples: Bool32,
+    pub sparse_residency_aliased: Bool32,
+    pub variable_multisample_rate: Bool32,
+    pub inherited_queries: Bool32,
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct VkExtent3D {
+    pub width: u32,
+    pub height: u32,
+    pub depth: u32,
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct VkQueueFamilyProperties {
+    pub queue_flags: Flags,
+    pub queue_count: u32,
+    pub timestamp_valid_bits: u32,
+    pub min_image_transfer_granularity: VkExtent3D
 }
 
 vk_define_handle!(VkInstance);
@@ -265,3 +285,4 @@ pub type vkDestroyInstance =  fn(instance: VkInstance, *const VkAllocationCallba
 pub type vkEnumeratePhysicalDevices = fn(instance: VkInstance, *mut uint32_t, *mut VkPhysicalDevice) -> VkResult;
 
 pub type vkCreateDevice = fn(physical_device: VkPhysicalDevice, *const DeviceCreateInfo, *const VkAllocationCallbacks, *mut VkDevice) -> VkResult;
+pub type vkGetPhysicalDeviceQueueFamilyProperties = fn(physical_device: VkPhysicalDevice, *mut uint32_t, *mut VkQueueFamilyProperties) -> ();
